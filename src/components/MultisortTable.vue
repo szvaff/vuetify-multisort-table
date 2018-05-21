37ef0374f9fee4272cdea91cbc74e3275e27c865
szvaff <pnagy90@gmail.com>
2018-05-21 21:42:32 +0200

    first steps2

---
+++ src/components/MultisortTable.vue
@@ -0,0 +1,109 @@
+<script>
+import VDataTable from 'vuetify/src/components/VDataTable'
+import VIcon from 'vuetify/src/components/VIcon'
+
+export default {
+  name: 'multisort-table',
+  mixins: [VDataTable],
+  data () {
+    return {
+      defaultPagination: {
+        page: 1,
+        rowsPerPage: 5,
+        sortBy: {},
+        totalItems: 0
+      }
+    }
+  },
+  created () {
+    this.defaultPagination.sortBy = {}
+    const firstSortable = this.headers.find(h => (
+      !('sortable' in h) || h.sortable)
+    )
+
+    if (!this.disableInitialSort && firstSortable) {
+      this.defaultPagination.sortBy[firstSortable.value] = true
+    }
+
+    this.initPagination()
+  },
+  methods: {
+    sort (index, shiftKey) {
+      var sortBy = this.computedPagination.sortBy
+      sortBy = sortBy === index ? {} : sortBy 
+
+      if (!shiftKey) {
+        for (var prop in sortBy) {
+          if (sortBy.hasOwnProperty(prop)) {
+            if (prop !== index) {
+              delete sortBy[prop]
+            }
+          }
+        }
+      }
+
+      if (!sortBy.hasOwnProperty(index)) {
+        sortBy[index] = true
+      } else if (sortBy[index] === true) {
+        sortBy[index] = false
+      } else if (sortBy[index] === false) {
+        sortBy[index] = null
+      } else {
+        sortBy[index] = true
+      }
+
+      this.updatePagination({ sortBy: sortBy })
+    },
+
+    genHeaderSortingData (header, children, data, classes) {
+      if (!('value' in header)) {
+        consoleWarn('Headers must have a value property that corresponds to a value in the v-model array', this)
+      }
+
+      data.attrs.tabIndex = 0
+      data.on = {
+        click: e => {
+          this.expanded = {}
+          this.sort(header.value, e.shiftKey)
+        },
+        keydown: e => {
+          // check for space
+          if (e.keyCode === 32) {
+            e.preventDefault()
+            this.sort(header.value)
+          }
+        }
+      }
+
+      classes.push('sortable')
+      const icon = this.$createElement(VIcon, {
+        props: {
+          small: true
+        }
+      }, this.sortIcon)
+      if (!header.align || header.align === 'left') {
+        children.push(icon)
+      } else {
+        children.unshift(icon)
+      }
+
+      const pagination = this.computedPagination
+      const beingSorted = pagination.sortBy.hasOwnProperty(header.value) && pagination.sortBy[header.value] !== null
+      if (beingSorted) {
+        classes.push('active')
+        if (pagination.sortBy[header.value] !== true) {
+          classes.push('desc')
+          data.attrs['aria-sort'] = 'descending'
+          data.attrs['aria-label'] += ': Sorted descending. Activate to remove sorting.' // TODO: Localization
+        } else {
+          classes.push('asc')
+          data.attrs['aria-sort'] = 'ascending'
+          data.attrs['aria-label'] += ': Sorted ascending. Activate to sort descending.' // TODO: Localization
+        }
+      } else {
+        data.attrs['aria-label'] += ': Not sorted. Activate to sort ascending.' // TODO: Localization
+      }
+    }
+  }
+}
+</script>
---
+++ src/main.js
@@ -0,0 +1,20 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+import App from './App'
+import Vuetify from 'vuetify'
+import 'vuetify/dist/vuetify.min.css'
+import MultisortTable from './components/MultisortTable'
+
+Vue.component('multisort-table', MultisortTable)
+
+Vue.use(Vuetify)
+
+Vue.config.productionTip = false
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  components: { App },
+  template: '<App/>'
+})
